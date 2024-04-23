8bf8f3fb4342d01d9fa314e7309fad34c99d54a4
Juan Andres Porras <juana.porras@upb.edu.co>
2024-04-22 22:27:27 -0500

    pergamino_screen full fix

--- lib/presentation/pergamino_screen/pergamino_screen.dart
+++ lib/presentation/pergamino_screen/pergamino_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import '../../core/app_export.dart';
-import '../../widgets/app_bar/appbar_leading_image.dart';
 import '../../widgets/app_bar/custom_app_bar.dart';
 import '../../widgets/custom_drop_down.dart';
 import 'widgets/nestedcolumns_item_widget.dart';
 import 'widgets/weatherinfo1_item_widget.dart'; // ignore_for_file: must_be_immutable
 
-// ignore_for_file: must_be_immutable
 class PergaminoScreen extends StatelessWidget {
   PergaminoScreen({Key? key})
       : super(
@@ -21,181 +19,126 @@ class PergaminoScreen extends StatelessWidget {
       child: Scaffold(
         appBar: _buildAppBar(context),
         body: Container(
+          padding: EdgeInsets.only(
+            left: 20.h,
+            right: 20.h,
+            top: 5.v,
+            bottom: 20.v,
+          ),
           width: double.maxFinite,
-          padding: EdgeInsets.symmetric(horizontal: 16.h),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               _buildListBox(context),
-              SizedBox(height: 14.v),
               Text(
                 "Pergamino #1",
                 style: CustomTextStyles.bodyLarge_1,
               ),
-              SizedBox(height: 14.v),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 17.h,
-                    right: 34.h,
-                  ),
-                  child: Row(
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
                     children: [
-                      Text(
-                        "Tiempo Estimado",
-                        style: theme.textTheme.bodyLarge,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          left: 33.h,
-                          bottom: 2.v,
-                        ),
-                        child: Text(
-                          "3d 12h",
-                          style: CustomTextStyles.bodyLargeBluegray700,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 3.v),
-              SizedBox(
-                height: 63.v,
-                width: 240.h,
-                child: Stack(
-                  alignment: Alignment.topLeft,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: SizedBox(
-                        width: 58.h,
-                        child: Text(
-                          "5 m/s - EW",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
-                          style: CustomTextStyles.bodyLargeBluegray700.copyWith(
-                            height: 1.25,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: GestureDetector(
-                        onTap: () {
-                          onTapTxtViento(context);
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 3.v),
+                      Expanded(
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
                           child: Text(
-                            "Viento",
+                            "Tiempo Estimado",
                             style: theme.textTheme.bodyLarge,
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          top: 37.v,
-                          right: 11.h,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                onTapTxtTemperatura(context);
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.only(bottom: 3.v),
-                                child: Text(
-                                  "Temperatura ",
-                                  style: theme.textTheme.bodyLarge,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 6.v),
-                              child: Text(
-                                "22°C",
-                                style: CustomTextStyles.bodyLargeBluegray700,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: 2.v),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 17.h,
-                    right: 43.h,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          onTapTxtHumedad(context);
-                        },
+                      Expanded(
                         child: Text(
-                          "Humedad",
-                          style: theme.textTheme.bodyLarge,
+                          "3d 12h",
+                          textAlign: TextAlign.center,
+                          style: CustomTextStyles.bodyLargeBluegray700,
                         ),
                       ),
-                      Text(
-                        "43%",
-                        style: CustomTextStyles.bodyLargeBluegray700,
-                      )
                     ],
                   ),
-                ),
-              ),
-              SizedBox(height: 3.v),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 17.h,
-                  right: 26.h,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        onTapTxtLuminocidad(context);
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 4.v),
+                  Row(
+                    children: [
+                      Expanded(
                         child: Text(
-                          "Luminocidad",
+                          "Viento",
                           style: theme.textTheme.bodyLarge,
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 7.v),
-                      child: Text(
-                        "21 W/m²",
-                        style: CustomTextStyles.bodyLargeBluegray700,
+                      Expanded(
+                        child: Text(
+                          "5 m/s - EW",
+                          textAlign: TextAlign.center,
+                          style: CustomTextStyles.bodyLargeBluegray700,
+                        ),
                       ),
-                    )
-                  ],
-                ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "Temperatura",
+                          style: theme.textTheme.bodyLarge,
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "22 °C",
+                          textAlign: TextAlign.center,
+                          style: CustomTextStyles.bodyLargeBluegray700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            onTapTxtHumedad(context);
+                          },
+                          child: Text(
+                            "Humedad",
+                            style: theme.textTheme.bodyLarge,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "43%",
+                          textAlign: TextAlign.center,
+                          style: CustomTextStyles.bodyLargeBluegray700,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            onTapTxtLuminocidad(context);
+                          },
+                          child: Text(
+                            "Luminocidad",
+                            style: theme.textTheme.bodyLarge,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "21 W/m²",
+                          textAlign: TextAlign.center,
+                          style: CustomTextStyles.bodyLargeBluegray700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(height: 16.v),
               _buildNestedColumns(context),
-              SizedBox(height: 18.v),
               _buildWeatherInfo(context)
             ],
           ),
@@ -207,49 +150,45 @@ class PergaminoScreen extends StatelessWidget {
   /// Section Widget
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return CustomAppBar(
-      leadingWidth: 319.h,
-      leading: AppbarLeadingImage(
-        imagePath: ImageConstant.imgArrowUp1ArrowUpKeyboard,
-        margin: EdgeInsets.fromLTRB(15.h, 6.v, 285.h, 7.v),
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back), // replace with your desired icon
+        iconSize: 30.0, // specify your desired size here
+        onPressed: () {
+          // handle your button tap here
+        },
       ),
     );
   }
 
   /// Section Widget
   Widget _buildListBox(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 17.h,
-        right: 19.h,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Pergaminos",
-            style: theme.textTheme.titleMedium,
-          ),
-          CustomDropDown(
-            icon: Container(
-              margin: EdgeInsets.symmetric(horizontal: 19.h),
-              child: CustomImageView(
-                imagePath: ImageConstant.imgArrowdown,
-                height: 3.v,
-                width: 8.h,
-              ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Pergaminos",
+          style: theme.textTheme.titleMedium,
+        ),
+        CustomDropDown(
+          icon: Container(
+            margin: EdgeInsets.symmetric(horizontal: 19.h),
+            child: CustomImageView(
+              imagePath: ImageConstant.imgArrowdown,
+              height: 3.v,
+              width: 8.h,
             ),
-            hintText: "Seelcciona el Pergamino",
-            items: dropdownItemList,
-          )
-        ],
-      ),
+          ),
+          hintText: "Seelcciona el Pergamino",
+          items: dropdownItemList,
+        )
+      ],
     );
   }
 
   /// Section Widget
   Widget _buildNestedColumns(BuildContext context) {
     return Container(
-      height: 107.v,
+      height: 100.v,
       padding: EdgeInsets.all(5.h),
       decoration: AppDecoration.outlineBlack900.copyWith(
         borderRadius: BorderRadiusStyle.roundedBorder5,
@@ -272,7 +211,7 @@ class PergaminoScreen extends StatelessWidget {
   /// Section Widget
   Widget _buildWeatherInfo(BuildContext context) {
     return Container(
-      height: 106.v,
+      height: 100.v,
       padding: EdgeInsets.all(5.h),
       decoration: AppDecoration.outlineBlack900.copyWith(
         borderRadius: BorderRadiusStyle.roundedBorder5,
